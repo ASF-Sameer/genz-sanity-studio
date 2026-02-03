05a2b69ab09abe4b7cb112173d0c47763c196a82
ASF-Sameer <abdulmohsenalzenki@gmail.com>
2026-02-03 13:16:29 +0300

    Add files via upload

---
+++ sanity.cli.ts
@@ -0,0 +1,9 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'nbpw4115',
+    dataset: 'production'
+  },
+  studioHost: 'genz-studio'
+})
